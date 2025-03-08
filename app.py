6c78185d35cec63b61aa0d04a8b8b431e7386805
Cassiana Gabriela Lima Barreto <lima.cglima@gmail.com>
2025-03-08 16:19:30 -0300

    updates app.py with new menu structure and image, adds instructions on home page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import streamlit_antd_components as sac
+from PIL import Image
 
 
 # Configuração da página
@@ -9,8 +10,42 @@ st.set_page_config(
     layout="wide",
 )
 
-st.title("Projeto Fome Zero")
+st.title("Fome Zero! Comida para todos")
+st.markdown("""---""")
 
-st.sidebar.title("Menu")
-menu_options = ["Main Page", "Countries", "Cities", "Cuisines"]
-choice = st.sidebar.selectbox("Selecione uma opção", menu_options)
+image = Image.open("/home/cassiana/repos/fomezero/images/cuisines.jpg")
+st.sidebar.image(image, width=250)
+
+# st.sidebar.title("Menu")
+menu_options = ["🧭 Home", "🗺️ Countries", "🏙️ Cities", "🍲 Cuisines"]
+choice = st.sidebar.selectbox("Menu", menu_options)
+st.sidebar.markdown("""---""")
+
+match choice:
+    case "🧭 Home":
+        # st.header("Home")
+        st.write("### Bem-vindo à página inicial")
+        st.markdown("""---""")
+        st.markdown(
+            """
+            ### Como utilizar este Dashboard?
+            - Escolha uma opção no menu à esquerda
+            - Em cada página, você encontrará informações sobre cada uma das Visões (Countries, Cities, Cuisines)
+            - Você pode filtrar as informações usando os filtros disponíveis
+            - Você pode clicar em um item para ver mais detalhes
+            """)
+    case "🗺️ Countries":
+        # st.header("Countries")
+        st.write("### Informações sobre os países")
+        st.markdown("""---""")
+    case "🏙️ Cities":
+        # st.header("Cities")
+        st.write("### Informações sobre as cidades")
+        st.markdown("""---""")
+    case "🍲 Cuisines":
+        # st.header("Cuisines")
+        st.write("### Informações sobre as cozinhas")
+        st.markdown("""---""")
+
+
+st.sidebar.markdown("### Powered by Cassiana Lima Barreto")
